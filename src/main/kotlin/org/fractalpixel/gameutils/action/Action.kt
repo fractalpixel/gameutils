8cdd90f066fbd6abcbbbf44d3e88c5326ea409b5
- <>
2019-11-03 20:56:23 +0200

    Started on actions

---
+++ src/main/kotlin/org/fractalpixel/gameutils/action/Action.kt
@@ -0,0 +1,10 @@
+package org.fractalpixel.gameutils.action
+
+/**
+ * Represents an action done by a player or AI controlled creature.
+ *
+ * Can be used to transfer commands from the client to the server.
+ */
+class Action {
+
+}
---
+++ src/main/kotlin/org/fractalpixel/gameutils/action/ActionService.kt
@@ -0,0 +1,20 @@
+package org.fractalpixel.gameutils.action
+
+import org.kwrench.service.ServiceBase
+import org.kwrench.service.ServiceProvider
+
+/**
+ * Queue and forward actions to the correct entity.
+ */
+class ActionService: ServiceBase() {
+
+    override fun doInit(serviceProvider: ServiceProvider?) {
+        TODO("not implemented")
+
+    }
+
+    override fun doShutdown() {
+        TODO("not implemented")
+
+    }
+}
