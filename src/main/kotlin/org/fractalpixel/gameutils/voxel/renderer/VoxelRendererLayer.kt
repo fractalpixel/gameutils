8ee4314d134e688f2b75e82bebb96083ee5c00ba
- <>
2020-02-14 01:29:15 +0200

    Added TODO:s.

--- src/main/kotlin/org/fractalpixel/gameutils/voxel/renderer/VoxelRendererLayer.kt
+++ src/main/kotlin/org/fractalpixel/gameutils/voxel/renderer/VoxelRendererLayer.kt
@@ -27,6 +27,11 @@ import kotlin.math.abs
  *   https://github.com/mikolalysenko/mikolalysenko.github.com/blob/master/Isosurface/js/surfacenets.js
  *   and https://github.com/TomaszFoster/NaiveSurfaceNets/blob/master/NaiveSurfaceNets.cs
  */
+// TODO: Convert to chunking approach, restrict number of voxels in chunk to less than size of Short, so that it works
+//       even in the worst case where a vertex is generated for each voxel (32*32*32 size should work with signed shorts,
+//       if vertexes are 0-based (check)).
+// TODO: Add level of detail
+// TODO: Add materials & texturing
 class VoxelRendererLayer(val terrain: VoxelTerrain): Layer3D() {
 
 
